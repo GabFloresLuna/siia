435642b19fdf0cd457103fb0a948484c46f215d1
GabFloresLuna <gab.floresl@duocuc.cl>
2026-06-13 00:35:53 -0400

    first commit

---
+++ src/main/java/cl/duocuc/siia/SiiaApplication.java
@@ -0,0 +1,13 @@
+package cl.duocuc.siia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duocuc/siia/SiiaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.siia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
